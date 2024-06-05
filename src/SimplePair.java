2c67c8697959fbebcc1f882731cc3b594f86d250
dawn_lch <chung4111@naver.com>
2024-06-05 18:52:45 +0900

    fd

---
+++ src/SimplePair.java
@@ -0,0 +1,14 @@
+public class SimplePair <T>{
+    private T first;
+    private T second;
+    public SimplePair(T t1, T t2){
+        first=t1;
+        second=t2;
+    }
+    public T getFirst(){
+        return first;
+    }
+    T getSecond(){
+        return second;
+    }
+}
---
+++ src/SimplePairTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SimplePairTest{
+    public static void main(String[] args) {
+        SimplePair<String> pair=new SimplePair<String>("apple","tomato");
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+    }
+}
